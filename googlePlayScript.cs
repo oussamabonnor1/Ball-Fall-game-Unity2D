21fad0ff6324ee69918feeb74e03dbecedc07f47
Oussama Bonnor <oussamabonnor@gmail.com>
2017-09-06 00:36:09 +0100

    Add files via upload

---
+++ googlePlayScript.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class googlePlayScript : MonoBehaviour
+{
+
+    public GameObject text;
+
+	// Use this for initialization
+	void Start () {
+		PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder().Build();
+	    PlayGamesPlatform.DebugLogEnabled = true;
+        PlayGamesPlatform.InitializeInstance(config);
+	    PlayGamesPlatform.Activate();
+	    GooglePlayGames.OurUtils.Logger.DebugLogEnabled = true;
+        signIn();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SignInCallback(bool success)
+    {
+        if (success)
+        {
+            // Change sign-in button text
+            text.GetComponent<Text>().text = "";
+        }
+        else
+        {
+            text.SetActive(false);
+        }
+    }
+
+    void signIn()
+    {
+        if (!Social.localUser.authenticated)
+        {
+            
+            text.GetComponent<Text>().text = "";
+            // Sign in with Play Game Services, showing the consent dialog
+            // by setting the second parameter to isSilent=false.
+            Social.localUser.Authenticate(SignInCallback);
+        }
+        else
+        {
+            text.GetComponent<Text>().text = "";
+        }
+
+    }
+
+    public void unlockAchievement(string id)
+    {
+        Social.ReportProgress(id,100,succes =>{});
+    }
+
+    public void showAchievementUI()
+    {
+        Social.ShowAchievementsUI();
+    }
+
+    public void addScoreLeaderbord(string id, int score)
+    {
+        Social.ReportScore(score,id, succes =>{});
+    }
+
+    public void showLeaderbordUI()
+    {
+        Social.ShowLeaderboardUI();
+    }
+}
